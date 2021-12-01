758c53c9a030b8977e628fe9b7ccbe5ff2046933
Wyatt726 <wyatt726@aol.com>
2021-12-01 12:43:42 -0500

    full tutorial ran through, still receiving error on post

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
-constrepoContext=require('./repository/repository-wrapper');
+const repoContext = require('./repository/repository-wrapper');
+const cors = require('cors');
+const validateProduct = require('./middleware/products-validation');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended:true}));
 
@@ -10,12 +13,27 @@ app.listen(3000, function() {
     console.log("Server started. Listening on port 3000.");
  });
 
-app.get('/api/products/:id',(req,res)=>{
-    constid=req.params.id;constproduct=repoContext.products.findProductById(id);
-    returnres.send(product);
+app.get('/api/products/:id',(req, res) => { 
+    const id = req.params.id;
+    const product = repoContext.products.findProductById(id);
+    return res.send(product);
 });
-app.post('/api/products',(req,res)=>{
-    constnewProduct=req.body;
-    constaddedProduct=repoContext.products.createProduct(newProduct);
-    returnres.send(addedProduct);
+
+app.post('/api/products', [validateProduct], (req, res) => {
+    const newProduct = req.body;
+    const addedProduct = repoContext.products.createProduct(newProduct);
+    return res.send(addedProduct);
+});
+
+app.put('/api/products/:id',[validateProduct], (req, res) => {
+    const id = req.params.id;
+    const productPropertiesToUpdate = req.body;
+    const updatedProduct = repoContext.products.updateProduct(id, productPropertiesToUpdate);
+    return res.send(updatedProduct)
+});
+
+app.delete('/api/products/:id',(req, res) => {
+    const id = req.params.id;
+    const updatedDataSet = repoContext.products.deleteProduct(id);
+    return res.send(updatedDataSet);
 });
