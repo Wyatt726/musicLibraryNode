3fae2b582443739ff0420250a26070521ea58663
Wyatt726 <wyatt726@aol.com>
2021-12-01 11:44:48 -0500

    adding in the get, post requests

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express');
+constrepoContext=require('./repository/repository-wrapper');
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended:true}));
 
 app.listen(3000, function() {
     console.log("Server started. Listening on port 3000.");
  });
 
+app.get('/api/products/:id',(req,res)=>{
+    constid=req.params.id;constproduct=repoContext.products.findProductById(id);
+    returnres.send(product);
+});
+app.post('/api/products',(req,res)=>{
+    constnewProduct=req.body;
+    constaddedProduct=repoContext.products.createProduct(newProduct);
+    returnres.send(addedProduct);
+});
