3dc1214fe448521bd7a5feede584374d62b3d014
Wyatt726 <wyatt726@aol.com>
2021-12-01 14:24:02 -0500

    updated endpoints to correcspond with song dataset

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const repoContext = require('./repository/repository-wrapper');
 const cors = require('cors');
-const validateProduct = require('./middleware/products-validation');
+const { validateSong } = require("./middleware/songs-validation.js")
 
 const app = express();
 
@@ -13,27 +13,27 @@ app.listen(3000, function() {
     console.log("Server started. Listening on port 3000.");
  });
 
-app.get('/api/products/:id',(req, res) => { 
+app.get('/api/songs/:id',(req, res) => { 
     const id = req.params.id;
-    const product = repoContext.products.findProductById(id);
-    return res.send(product);
+    const song = repoContext.songs.findSongById(id);
+    return res.send(song);
 });
 
-app.post('/api/products', [validateProduct], (req, res) => {
-    const newProduct = req.body;
-    const addedProduct = repoContext.products.createProduct(newProduct);
-    return res.send(addedProduct);
+app.post('/api/songs', [validateSong], (req, res) => {
+    const newSong = req.body;
+    const addedSong = repoContext.songs.createSong(newSong);
+    return res.send(addedSong);
 });
 
-app.put('/api/products/:id',[validateProduct], (req, res) => {
+app.put('/api/songs/:id',[validateSong], (req, res) => {
     const id = req.params.id;
-    const productPropertiesToUpdate = req.body;
-    const updatedProduct = repoContext.products.updateProduct(id, productPropertiesToUpdate);
-    return res.send(updatedProduct)
+    const songPropertiesToUpdate = req.body;
+    const updatedSong = repoContext.songs.updateSong(id, songPropertiesToUpdate);
+    return res.send(updatedSong)
 });
 
-app.delete('/api/products/:id',(req, res) => {
+app.delete('/api/songs/:id',(req, res) => {
     const id = req.params.id;
-    const updatedDataSet = repoContext.products.deleteProduct(id);
+    const updatedDataSet = repoContext.songs.deleteSong(id);
     return res.send(updatedDataSet);
 });
